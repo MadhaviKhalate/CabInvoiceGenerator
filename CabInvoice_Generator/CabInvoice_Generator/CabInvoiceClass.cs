f8512d69c66517c4dd281079211b18b88fd21538
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-06-22 17:49:35 +0530

    [Madhavi] Calculating Total Fare

---
+++ CabInvoice_Generator/CabInvoice_Generator/CabInvoiceClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoice_Generator
+{
+    public class CabInvoiceClass
+    {
+        private readonly double MINIMUM_COST_PER_KM = 10;
+        private readonly int COST_PER_TIME = 1;
+        private readonly double MINIMUM_FARE = 5;
+        public double distance;
+        public int time;
+        public CabInvoiceClass(double distance, int time)
+        {
+            this.distance = distance;
+            this.time = time;
+        }
+        public double CalculateFare()
+        {
+            double totalFare = 0;
+            if (distance >= 0 && time >= 0)
+            {
+                totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+            }
+            else
+            {
+                if (distance <= 0)
+                {
+                    throw new CustomException(CustomException.ExceptionType.INVALID_DISTANCE, "Invalid Distance");
+                }
+                if (time < 0)
+                {
+                    throw new CustomException(CustomException.ExceptionType.INVALID_TIME, "Invalid Time");
+                }
+            }
+            return totalFare;
+        }
+    }
+}
---
+++ CabInvoice_Generator/CabInvoice_Generator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CabInvoice_Generator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CabInvoiceClass getMethod = new CabInvoiceClass(10.0, 5);
+            double fare = getMethod.CalculateFare();
+            Console.WriteLine("Fare: " + fare);
+        }
+    }
+}
